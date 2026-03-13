4e1d1e3ad7f05dae8795e21c901cb8a3dbd2f59b
mnm189186-cpu <mnm189186@gmail.com>
2026-03-13 04:16:37 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
+
 # تحميل متغيرات من .env إن وجدت
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
+
+# إنشاء مجلد التنزيلات
+mkdir -p /app/downloads
+
+# تشغيل البوت
 python bot.py
-RUN mkdir -p /app/downloads && chown -R 1000:1000 /app/downloads
-
-ENV PYTHONUNBUFFERED=1
-
-CMD ["./run.sh"]

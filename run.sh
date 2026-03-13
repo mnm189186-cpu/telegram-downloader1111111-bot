6a7357847bf2cf5615d109f141bc4535909aefe5
mnm189186-cpu <mnm189186@gmail.com>
2026-03-13 04:07:17 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,8 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 python bot.py
+RUN mkdir -p /app/downloads && chown -R 1000:1000 /app/downloads
+
+ENV PYTHONUNBUFFERED=1
+
+CMD ["./run.sh"]
